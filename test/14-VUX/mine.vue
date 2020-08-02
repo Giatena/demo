c4652eaa5bf2bb635152fd8858110454f294b29e
Giatena <zhu_ce_you_xiang@yeah.net>
2020-08-02 22:50:02 +0800

    v0.2.15

--- test/14-VUX/mine.vue
+++ test/14-VUX/mine.vue
@@ -1,66 +1,70 @@
 <template>
-  <div class="mine">
-  	<h1>电子工单</h1>
-    <group :title="1">
-      <x-input title="质保工单" v-model="ticket" placeholder="xxxxxxxxCW_基地_xx"></x-input>
-      <x-input title="列车编号" v-model="train" type="number"></x-input>
-      <x-input title="车厢编号" v-model="compartment" type="number"></x-input>
-      <x-input title="故障类别" v-model="category" readonly=true></x-input>
-      <checker v-model="category" default-item-class="checker-item" selected-item-class="checker-item-selected">
-        <checker-item value="辅逆">辅逆</checker-item>
-        <checker-item value="牵引">牵引</checker-item>
-        <checker-item value="列控">列控</checker-item>
-      </checker>
-      <x-input title="故障类别" v-model="type" readonly=true></x-input>
-      <checker v-model="type" default-item-class="checker-item" selected-item-class="checker-item-selected">
-        <checker-item value="救援">救援</checker-item>
-        <checker-item value="清客">清客</checker-item>
-        <checker-item value="掉线">掉线</checker-item>
-        <checker-item value="换表">换表</checker-item>
-        <checker-item value="无影响">无影响</checker-item>
-        <checker-item value="均修">均修</checker-item>
-      </checker>
-      <x-textarea title="故障现象" v-model="description" :max="2000" autosize=true></x-textarea>
-    </group>
-    <group :title="2">
-      <x-input title="故障位置" v-model="fault_compartment" type="number"></x-input>
-      <x-textarea title="处理经过" v-model="action" :max="2000" autosize=true></x-textarea>
-      <x-input title="分厂报单" v-model="user_ticket" type="number"></x-input>
-      <x-input title="当班人员" v-model="member"></x-input>
-    </group>
-    <br><br>
-  </div>
+	<div class="mine">
+		<h2 align="center">电子工单</h2>
+		<group :title="1">
+			<x-input title="质保工单" v-model="ticket" placeholder="xxxxxxxxCW_基地_xx"></x-input>
+			<x-input title="列车编号" v-model="train" type="number"></x-input>
+			<x-input title="车厢编号" v-model="compartment" type="number"></x-input>
+			<x-input title="故障类别" v-model="category" readonly=true></x-input>
+			<checker v-model="category" default-item-class="checker-item" selected-item-class="checker-item-selected">
+				<checker-item value="辅逆">辅逆</checker-item>
+				<checker-item value="牵引">牵引</checker-item>
+				<checker-item value="列控">列控</checker-item>
+			</checker>
+			<x-input title="故障类别" v-model="type" readonly=true></x-input>
+			<checker v-model="type" default-item-class="checker-item" selected-item-class="checker-item-selected">
+				<checker-item value="救援">救援</checker-item>
+				<checker-item value="清客">清客</checker-item>
+				<checker-item value="掉线">掉线</checker-item>
+				<checker-item value="换表">换表</checker-item>
+				<checker-item value="无影响">无影响</checker-item>
+				<checker-item value="均修">均修</checker-item>
+			</checker>
+			<x-textarea title="故障现象" v-model="description" :max="2000" autosize=true></x-textarea>
+		</group>
+		<group :title="2">
+			<x-input title="故障位置" v-model="fault_compartment" type="number"></x-input>
+			<x-textarea title="处理经过" v-model="action" :max="2000" autosize=true></x-textarea>
+			<x-input title="分厂报单" v-model="user_ticket" type="number"></x-input>
+			<x-input title="更换或对翻情况" v-model="replacement"></x-input>
+			<x-input title="当班人员" v-model="member"></x-input>
+		</group>
+		<br><br>
+
+	</div>
 </template>
 
 <script>
-import { Group, Cell, XInput, XTextarea, Checker, CheckerItem, Divider, PopupPicker, PopupRadio } from 'vux'
+import { Group, Cell, XInput, XTextarea, Checker, CheckerItem, Divider, PopupPicker, PopupRadio, FormPreview } from 'vux'
 
 export default {
-  components: {
-    Group,
-    Cell,
-    XInput,
-    XTextarea,
-    Checker,
-    CheckerItem,
-    Divider,
-    PopupPicker,
-    PopupRadio
-  },
-  data () {
-    return {
-		  ticket: '',
-      ticket_checked: '',
-      train: '',
-      compartment: '',
-      category: '',
-      type: '',
-      fault_compartment: '',
-      fault_compartment_checked: false,
-      description: '',
-      action: '',
-      user_ticket: '',
-      member: ''
+	components: {
+		Group,
+		Cell,
+		XInput,
+		XTextarea,
+		Checker,
+		CheckerItem,
+		Divider,
+		PopupPicker,
+		PopupRadio,
+		FormPreview 
+	},
+	data () {
+		return {
+			ticket: '',
+			ticket_checked: '',
+			train: '',
+			compartment: '',
+			category: '',
+			type: '',
+			fault_compartment: '',
+			fault_compartment_checked: false,
+			description: '',
+			action: '',
+			replacement: '',
+			user_ticket: '',
+			member: ''
 		}
 	}
 }
@@ -68,13 +72,13 @@ export default {
 
 <style scoped>
 .box {
-  padding: 0 15px;
+	padding: 0 15px;
 }
 .checker-item {
-  border: 1px solid #ececec;
-  padding: 5px 15px;
+	border: 1px solid #ececec;
+	padding: 5px 15px;
 }
 .checker-item-selected {
-  border: 1px solid green;
+	border: 1px solid green;
 }
 </style>
---
+++ test/14-VUX/personalCourses.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="personalCourses">
+		<br>
+		<x-input title="导入" v-model="userStr"></x-input>
+		<x-button mini plain @click.native="clickButton($event)">预览</x-button>
+		<br><br>
+
+    	<divider>mini</divider>
+		<x-button mini>submit</x-button>
+		<x-button mini type="primary">primary</x-button>
+		<x-button mini type="warn">Delete</x-button>
+		<br>
+		<x-button mini plain>submit</x-button>
+		<x-button mini plain type="primary">primary</x-button>
+		<x-button mini plain type="warn">primary</x-button>
+	</div>
+</template>
+
+<script>
+import { Divider, XButton, XInput, FormPreview } from 'vux'
+
+export default {
+	components: {
+		Divider,
+		XButton,
+		XInput,
+		FormPreview
+	},
+	data () {
+		return {
+			userStr: '',
+			str2: ''
+		}
+	},
+	watch: {
+		userStr() {
+			// 实时监视变量userStr的变化，每输入一个字符，调用一次
+			console.log("watch()");
+		}
+	},
+	methods: {
+		clickButton: function(event) {
+			var str = {"GUID":"20200231230315","ticket":"20200231CW_ML_01","train":"999","compartment":"999999","category":"1","type":"5","description":"牵引轻级","action":"xxxxxxxx，工况正常。","replacement":"1","component":["PIM1","PIM2","IM"],"source":["电科","甲供","申南"],"old":["01","02","03"],"new":["1N","2N","3N"]}
+			console.log("str(预设)：" + str);
+			str = this.userStr
+			console.log("userStr：" + this.userStr);
+			if(typeof str == "object") {
+				str = JSON.stringify(str);
+			};
+			navigator.clipboard.writeText(str);
+			console.log("已导出到剪贴板：" + str);
+		},
+	},
+}
+</script>
+
+<style>
+</style>
